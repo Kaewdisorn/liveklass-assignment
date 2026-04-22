5b623d8e102361120f466704ca8b33480b45d61c
Nane <kds.napha@gmail.com>
2026-04-23 01:30:49 +0900

    enrollment entity 생성

---
+++ src/main/java/com/example/liveklass/enrollment/entity/Enrollment.java
@@ -0,0 +1,87 @@
+package com.example.liveklass.enrollment.entity;
+
+import java.time.OffsetDateTime;
+
+import com.example.liveklass.enrollment.enums.EnrollmentStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "enrollment")
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "course_id", nullable = false)
+    private Long courseId;
+
+    @Column(name = "student_id", nullable = false)
+    private Long studentId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private EnrollmentStatus status;
+
+    @Column(name = "requested_at", nullable = false, updatable = false)
+    private OffsetDateTime requestedAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public EnrollmentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnrollmentStatus status) {
+        this.status = status;
+    }
+
+    public OffsetDateTime getRequestedAt() {
+        return requestedAt;
+    }
+
+    public void setRequestedAt(OffsetDateTime requestedAt) {
+        this.requestedAt = requestedAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
