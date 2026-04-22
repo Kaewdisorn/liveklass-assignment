0d85b3d3c602ae5f8d4b56ad439e0a5177302e7d
Nane <kds.napha@gmail.com>
2026-04-22 18:00:39 +0900

    테스트 디렉토리 추가

---
+++ src/test/java/com/example/liveklass/support/IntegrationTestSupport.java
@@ -0,0 +1,26 @@
+package com.example.liveklass.support;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class IntegrationTestSupport {
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("liveklass_test")
+            .withUsername("liveklass")
+            .withPassword("liveklass");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
