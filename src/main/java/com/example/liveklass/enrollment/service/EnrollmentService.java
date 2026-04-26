8c0ce7d51ec5cb884c3d377966ee3ecd37734885
Nane <kds.napha@gmail.com>
2026-04-26 19:47:26 +0900

    수강 신청 인터페이스 수정

--- src/main/java/com/example/liveklass/enrollment/service/EnrollmentService.java
+++ src/main/java/com/example/liveklass/enrollment/service/EnrollmentService.java
@@ -8,5 +8,15 @@ import com.example.liveklass.enrollment.dto.EnrollmentResponse;
 
 public interface EnrollmentService {
 
+    // 수강 신청
     EnrollmentResponse createEnrollment(RequestUser requestUser, CreateEnrollmentRequest request);
+
+    // 수강 신청 승인
+    EnrollmentResponse confirmEnrollment(RequestUser requestUser, Long enrollmentId);
+
+    // 수강 신청 취소
+    EnrollmentResponse cancelEnrollment(RequestUser requestUser, Long enrollmentId);
+
+    // 내 수강 신청 조회
+    List<EnrollmentResponse> getMyEnrollments(RequestUser requestUser);
 }
