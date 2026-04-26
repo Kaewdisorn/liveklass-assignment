79ce56cdf0db95dff8af75d640be4d5d51f342ae
Nane <kds.napha@gmail.com>
2026-04-26 09:44:41 +0900

    CourseRepository 수정

--- src/main/java/com/example/liveklass/course/repository/CourseRepository.java
+++ src/main/java/com/example/liveklass/course/repository/CourseRepository.java
@@ -1,11 +1,23 @@
 package com.example.liveklass.course.repository;
 
 import java.util.List;
+import java.util.Optional;
+
+import jakarta.persistence.LockModeType;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.liveklass.course.entity.Course;
+import com.example.liveklass.course.enums.CourseStatus;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    List<Course> findByStatus(CourseStatus status);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Course c where c.id = :courseId")
+    Optional<Course> findByIdForUpdate(@Param("courseId") Long courseId);
 }
